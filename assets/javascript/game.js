1cc993a2fc7bc824fe0386c10b1a44736075729e
Val Gabriel <gabriel.valryan@hotmail.com>
2018-04-07 08:44:02 -0500

    header

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+// Need to make condition to not allow keys that are not letters;
+// Need condition to avoid taking away guesses for choosing the same letter twice.
+
 // game variables
 var charList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var lettersGuessed = [];
